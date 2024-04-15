41f7917a86b3a5adbe3226f5f42b8208dbb88d33
#Jiawei Sun# <alinajw@bu.edu>
2024-04-15 15:02:24 -0400

    added expiration check

--- subguardian/subdomain_check/mx_check.py
+++ subguardian/subdomain_check/mx_check.py
@@ -1,5 +1,7 @@
 import smtplib
 import ssl
+import whois
+import datetime
 
 def check_mx_connect(mx_record):
     # Attempt to connect to the SMTP server
@@ -31,12 +33,37 @@ def check_mx_connect(mx_record):
     except Exception as e:
         print(f"Error connecting to {smtp_server} with ip {ip} on port {port}: {e}")
 
+def check_if_expired(mx_record):
+    mx_domain = str(mx_record['exchange'])
+    mx_domain = mx_domain.split('.')[-2] + '.' + mx_domain.split('.')[-1]
 
+    try:
+        mx_whois = whois.whois(mx_domain)
+        if isinstance(mx_whois.expiration_date, list):
+            expiration_date = mx_whois.expiration_date[0]
+        else:
+            expiration_date = mx_whois.expiration_date
+
+        if expiration_date and expiration_date < datetime.datetime.now():
+            print(f"MX domain {mx_domain} is expired")
+            return True
+        else:
+            print(f"MX domain {mx_domain} has not expired")
+            return False
+    except whois.parser.PywhoisError:
+        print(f"WHOIS data not found for MX domain {mx_domain}")
+        return False
+    
 def mx_check(mx_records):
     vulnerability = {}
     for mx_record in mx_records:
         connect_result = check_mx_connect(mx_record)
         if connect_result == "TSL":
             vulnerability[mx_record['exchange']] = "Might Lack of support for STARTTLS encryption."
+
+        # Check for expired domain for current NS record
+        expired = check_if_expired(mx_record)
+        if expired:
+            vulnerability[mx_record['exchange']] = 'expired'
             
-    return
+    return vulnerability
