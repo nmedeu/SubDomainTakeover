d4a702b59558d485952aad38d6c15a54dc550df8
Jeet <jeetv@bu.edu>
2024-04-27 22:41:41 -0400

    merge fix

--- subguardian/cli.py
+++ subguardian/cli.py
@@ -350,14 +350,12 @@ def main():
 
 
     
-<<<<<<< HEAD
     # Delete vulnerable records if possible
     vulnerable_subdomains = []
     if types:
         for host in types:
             if host == 'cloudfare':
                 cloudfare_prevention(vulnerable_subdomains)
-=======
     # Jeetcreates.com
     #records = {'A': [{'address': '185.230.63.171', 'name': 'jeetcreates.com', 'type': 'A'}, {'address': '185.230.63.171', 'domain': 'jeetcreates.com', 'name': 'jeetcreates.com', 'type': 'A'}, {'address': '185.230.63.107', 'name': 'jeetcreates.com', 'type': 'A'}, {'address': '185.230.63.186', 'domain': 'jeetcreates.com', 'name': 'jeetcreates.com', 'type': 'A'}, {'address': '185.230.63.107', 'domain': 'jeetcreates.com', 'name': 'jeetcreates.com', 'type': 'A'}, {'address': '185.230.63.186', 'name': 'jeetcreates.com', 'type': 'A'}], 'NS': [{'Version': '', 'address': '216.239.38.100', 'domain': 'jeetcreates.com', 'recursive': 'True', 'target': 'ns3.wixdns.net', 'type': 'NS'}, {'Version': '', 'address': '216.239.36.100', 'domain': 'jeetcreates.com', 'recursive': 'True', 'target': 'ns2.wixdns.net', 'type': 'NS'}], 'SOA': [{'address': '216.239.36.100', 'domain': 'jeetcreates.com', 'mname': 'ns2.wixdns.net', 'type': 'SOA'}], 'sublist3r': [{'name': 'www.jeetcreates.com', 'type': 'subdomain'}]}
     
@@ -394,4 +392,3 @@ def main():
 
 
     
->>>>>>> 74983c2836bb1c41e69c0b34976361c9feda0e06
